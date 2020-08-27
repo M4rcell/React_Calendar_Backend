b25d4d9ad3138f091f972c756bbd391f3b2ecfe8
Marcel <halanocca21@gmail.com>
2020-08-27 11:12:54 -0500

    Validar campos tienes que ser obligatios y validar deacuerdo del campos necesario

--- routers/eventsCRUD.js
+++ routers/eventsCRUD.js
@@ -4,23 +4,35 @@
 */
 
 const {Router} = require('express'); 
-const router=Router();
+const {check}  = require('express-validator');
+const {validarCampos} = require('../middlewares/validar-campos')
 
 // Todas tienes que pasar por la  validacion del JWT
 const {validarJWT} = require('../middlewares/validar-jwt')
-
+const {isDate} = require('../helpers/isDate')
 //traendo funciones desde el controlador
 const {getEventos,crearEvento,actualizarEvento,eliminarEvento} = require('../controllers/eventsCRUD')
 
+const router=Router();
 // validar todas rutas sean protegidas
 router.use(validarJWT); //es ingual a router.get('/',validarJWT,getEventos);
 
 // Obtener eventos
-router.get('/',getEventos);
+router.get('/',
+
+getEventos);
 
 
 // crear un nuevo evento
-router.post('/',crearEvento);
+router.post('/',
+  [
+    check('title','El titulo es obligatorio ').not().isEmpty(),
+    check('start','Fecha de inicio es obligatorio').custom(isDate),
+    check('end','Fecha de finalizacion es obligatorio').custom(isDate),
+    
+    validarCampos,
+  ],
+  crearEvento);
 
 // Actualizar evento
 router.put('/:id',actualizarEvento);
